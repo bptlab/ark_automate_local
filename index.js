1987f7147e1f9c3c3b0152fcbd24849b91d63b1f
DanielWoelki <daniel.woelki@outlook.de>
2021-03-16 19:36:04 +0100

    basic socket.io implementation based on node js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-import { io } from 'socket.io-client';
+const fs = require('fs');
+const exec = require('child_process').exec;
+const io = require('socket.io-client');
 
 const socket = io('http://localhost:5001');
 
 socket.on('connect', () => {
   console.log('Client: ', socket.id);
+});
+
+socket.on('newRobot', (robotCode) => {
+  //console.log('newRobot: ', robotCode);
+  fs.writeFile('./executable.robot', robotCode, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('The file was saved!');
+  });
+  exec('robot executable.robot');
 });
