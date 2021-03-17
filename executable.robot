2aac1ee8605973b40ef031e1d6566c94c686f0af
DanielWoelki <daniel.woelki@outlook.de>
2021-03-17 11:14:01 +0100

    implemented rooms (userId still hardcoded)

--- executable.robot
+++ executable.robot
@@ -1,8 +1,1 @@
-*** Settings ***
-Documentation  Our first parsed RPA
-Library    RPA.Excel.Files
-Library    RPA.Browser
 
-*** Tasks ***
-SecondActivity
-  Find Empty Row  TestString
--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const exec = require('child_process').exec;
 const io = require('socket.io-client');
 
 const socket = io('http://localhost:5001');
+const userId = '80625d115100a2ee8d8e695b';
 
 socket.on('connect', () => {
   console.log('Client: ', socket.id);
+  socket.emit('joinUserRoom', userId);
+});
+
+socket.on('successUserRoomConnection', (message) => {
+  console.log(message);
+});
+socket.on('errorUserRoomConnection', (message) => {
+  console.log(message);
+});
+socket.on('newClientJoinedUserRoom', (message) => {
+  console.log(message);
 });
 
 socket.on('robotExecution', (robotCode) => {
